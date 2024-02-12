d12de1bd6e1f96761b513f2d8d3354791b4de21b
MaximoRdz <mxrdhr@gmail.com>
2024-02-12 18:43:03 +0100

    Comparing Recursive Runtimes

--- RECURSION/recursion.py
+++ RECURSION/recursion.py
@@ -1,3 +1,20 @@
+import time
+
+
+def runtime(func):
+
+    def wrapper(*args, **kwargs):
+        dt = 0
+        N = 10
+        for _ in range(N):
+            t1 = time.time()
+            _ = func(*args, **kwargs)
+            dt += time.time() - t1
+
+        return dt / N
+    
+    return wrapper
+
 def multiply_by_sum(a: int, b: int) -> int: 
     """Naive implementation."""
     if b == 0:
@@ -25,9 +42,58 @@ def binary_search(array: list, item):
         return binary_search(array[:mid], item)
 
 
+@runtime
+def sum_list(array: list):
+    """
+    Highly inefficient recursive way of summing a list.
+    Creating a new array every time the function makes a
+    recursive call is not good. spacial complexity ^
+    """
+   
+    if len(array) == 0:
+        return 0
+    
+    result = array[0]
 
+    result += sum_list(array[1:])
+
+    return result
+
+
+@runtime
+def sum_list_pop(array: list):
+    """More efficient algorithm."""
+
+    if len(array) == 0:
+        return 0
+    
+    result = 0
+
+    result = array.pop() + sum_list(array)
+
+    return result
+
+
+@runtime
+def sum_list_index(array: list, ind: int):
+
+    if ind == len(array):
+        return 0
+    
+    result = array[ind]
+    result += sum_list_index(array, ind+1)
+
+    return result
+    
 
 if __name__ == "__main__":
-    print(multiply_by_sum(4, 5))
+    print("Multiply by sum: ", multiply_by_sum(4, 5))
 
-    print(binary_search([1, 2, 3, 4, 5], 5))
+    print("Binary search: ", binary_search([1, 2, 3, 4, 5], 5))
+
+    print(f"sum list: {sum_list([1, 2, 3, 4, 5]):5f} s")
+
+    print(f"sum list pop: {sum_list_pop([1, 2, 3, 4, 5]):5f} s")
+
+    print(f"sum list ind: {sum_list_index([1, 2, 3, 4, 5], 0):5f} s")
+
