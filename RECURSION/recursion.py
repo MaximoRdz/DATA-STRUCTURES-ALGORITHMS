65efaa8723e1b1a1c542b287d5d6b374b6042bb7
MaximoRdz <mxrdhr@gmail.com>
2024-02-12 18:22:13 +0100

    Recursive implementation of Binary Search

--- RECURSION/recursion.py
+++ RECURSION/recursion.py
@@ -8,12 +8,26 @@ def multiply_by_sum(a: int, b: int) -> int:
     return result
 
 
-def binary_search(item, array: list, start, end):
-    """Given a sorted array."""
-    pass
+def binary_search(array: list, item):
+    """Given a sorted array. Recusive binary search."""
+    left, right = 0, len(array) - 1
+    mid = (right + left) // 2
+
+    print(f"looking for {item} in {array}")
+
+    if array[mid] == item:
+        return True
+    
+    elif array[mid] < item:
+        return binary_search(array[mid+1:], item)
+    
+    else:
+        return binary_search(array[:mid], item)
 
 
 
 
 if __name__ == "__main__":
-    print(multiply_by_sum(4, -5))
+    print(multiply_by_sum(4, 5))
+
+    print(binary_search([1, 2, 3, 4, 5], 5))
