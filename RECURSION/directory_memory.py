65ca97c9a75a69bab6a9b2b7b597ea36f0446e13
MaximoRdz <mxrdhr@gmail.com>
2024-02-13 17:49:52 +0100

    Recursively Computes Memory of a Directory

--- RECURSION/directory_memory.py
+++ RECURSION/directory_memory.py
@@ -6,25 +6,23 @@ def memory_use(path):
     # if file return size and sum, if folder keep recursion
 
     if os.path.isfile(path):
-        print("file: ", path, "visited")
         return os.path.getsize(path)
-    
-    if os.path.isdir(path):
-        print("dir: ", path, "visited")
-        return memory_use(path)
-    
+
     memory = 0
-    memory += memory_use(path)
+
+    if os.path.isdir(path):
+        for child in os.listdir(path):
+            memory += memory_use(os.path.join(path, child))
 
     return memory
-    
+
 
 def main(args):
     assert os.path.isdir(args.fpath), "Path Format Is Not Dir"
     assert os.path.exists(args.fpath), "Path Does Not Exist"
 
-    print(f"\n{args.fpath}\n\nMemory: {memory_use(args.fpath)} bytes\n")
-    
+    print(f"\n{args.fpath}\tMemory: {memory_use(args.fpath)/1e6 : .2f} Mb\n")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
