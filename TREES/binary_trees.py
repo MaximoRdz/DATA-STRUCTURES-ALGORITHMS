bc166a04a255dcccbe24b1a3b465a5d3e853f832
MaximoRdz <mxrdhr@gmail.com>
2024-02-15 18:54:29 +0100

    +1 day

--- TREES/binary_trees.py
+++ TREES/binary_trees.py
@@ -14,11 +14,14 @@ class BinaryNode:
 
 
 class BinaryTree:
-    def __init__(self, root: "BinaryNode" = None) -> None:
+    def __init__(self, root: BinaryNode = None) -> None:
         self._root = root
 
     def __len__(self):
-        "Size of a Tree (number of nodes)."
+        """
+        Size of a Tree (number of nodes). Iterative Implementation
+        PreOrder traversal.
+        """
         if self._root is None:
             return 0
         
@@ -42,7 +45,55 @@ class BinaryTree:
                 stack.append(right)
         
         return size
+    
+    def size(self):
+        return self._size(self._root)
 
+    def _size(self, node: BinaryNode) -> int:
+        """
+        Recursive node counting
+        """
+        if node is None:
+            return 0
+        
+        return 1 + self._size(node.left) + self._size(node.right)
+    
+    def height(self):
+        return self._height(self._root)
+
+    def _height(self, node: BinaryNode) -> int:
+        "Longest path from a node to a leaf."
+        if node is None:
+            return -1
+        
+        return 1 + max(self._height(node.left), self._height(node.right))
+    
+    def preorder(self) -> None:
+        print("\nPreorder Traversal Recursive:")
+        self._preorder(self._root)
+        print()
+
+    def _preorder(self, node: BinaryNode) -> None:
+        """
+        Prints the preorder traversal of the tree (recursive implementation)
+        first visited marked or (root, left, right).
+        """
+        if node is not None:
+            print(node.elem, end=" ")       
+            self._preorder(node.left)
+            self._preorder(node.right)
+        
+    def inorder(self) -> None:
+        print("\nInorder Traversal Recursive:")
+        self._inorder(self._root)
+        print()
+    
+    def _inorder(self, node: BinaryNode) -> None:
+        "second visited or (left, root, right)"
+        if node is not None:
+            print(node.elem, end=" ")       
+            self._preorder(node.left)
+            self._preorder(node.right)
 
     def pre_order_traversal(self):
         """Pre Order traversal by default."""
@@ -70,46 +121,6 @@ class BinaryTree:
         
         return path
     
-    def in_order_traversal(self):
-        """In Order traversal by default."""
-        if self._root is None:
-            raise IndexError("Empty Tree Cannot Be Traversed.")
-
-        node = self._root
-        path = []
-        
-        stack = []
-        stack.append(node)
-
-        visited = set()
-
-        while stack:
-            node = stack.pop()
-
-            if node in visited:
-                path.append(node.elem)
-            else:
-                stack.append(node)
-
-            visited.add(node)
-
-            left = node.left
-            right = node.right
-
-            if right is not None:
-                stack.append(right)
-            
-            if left is not None:
-                stack.append(left)
-        
-        return path
-
-
-
-
-    def __str__(self, mode="") -> str:
-        pass
-
 
 if __name__ == "__main__":
     #             10
@@ -132,6 +143,12 @@ if __name__ == "__main__":
 
     tree = BinaryTree(root)
 
-    print(tree.in_order_traversal())
+    print("Tree Length: ", len(tree))
+    print("Tree Length Recursive: ", tree.size())
+    print("Tree Height: ", tree.height())
+
+    print("PreOrder Stack: ", tree.pre_order_traversal())
+    tree.preorder()
+    tree.inorder()
 
 
